ff04c08cd5b4c25dcc41d764897457618e447318
Pablo Nuñez Pölcher <pnpolcher@gmail.com>
2021-03-26 00:28:57 +0100

    Bug fixes. Routing tests.

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -1,10 +1,14 @@
+from functools import partial
 import itertools
-from typing import ClassVar, List, Set
+from typing import Any, ClassVar, Dict, List, Optional
 import unittest
 
+from pyrazine.context import RequestContext
 from pyrazine.exceptions import MethodNotAllowedError
 from pyrazine.response import HttpResponse
 from pyrazine.routing import Route, Router
+from pyrazine.jwt import JwtToken
+from tests.helpers import get_access_token
 
 
 class TestRoute(unittest.TestCase):
@@ -14,16 +18,16 @@ class TestRoute(unittest.TestCase):
     ]
 
     @staticmethod
-    def _mock_failure_authorizer(roles: Set[str]) -> bool:
+    def _mock_failure_authorizer(roles: List[str], token: JwtToken, context: Optional[Dict[str, Any]]) -> bool:
         return False
 
     @staticmethod
-    def _mock_success_authorizer(roles: Set[str]) -> bool:
+    def _mock_success_authorizer(roles: List[str], token: JwtToken, context: Optional[Dict[str, Any]]) -> bool:
         return True
 
     @staticmethod
-    def _mock_handler():
-        return HttpResponse()
+    def _mock_handler(token, body, ctx, return_value: int):
+        return HttpResponse(200, {'return_value': return_value})
 
     def test_route_methods(self):
         for repeats in range(1, len(TestRoute._VALID_METHODS) + 1):
@@ -67,20 +71,20 @@ class TestRoute(unittest.TestCase):
         router = Router()
 
         # Add a root route without an authorizer
-        router.add_route(['GET', 'POST'], '/', self._mock_handler)
+        router.add_route(['GET', 'POST'], '/', partial(self._mock_handler, return_value=-1))
 
         # Add a route with a simple path with a success authorizer.
         router.add_route(
             ['GET', 'POST'],
             '/submit/success_path',
-            self._mock_handler,
+            partial(self._mock_handler, return_value=0),
             self._mock_success_authorizer)
 
         # Add a route with a simple path with a failure authorizer.
         router.add_route(
             ['GET', 'POST'],
             '/submit/failure_path',
-            self._mock_handler,
+            partial(self._mock_handler, return_value=1),
             self._mock_failure_authorizer
         )
 
@@ -88,6 +92,21 @@ class TestRoute(unittest.TestCase):
         router.add_route(
             ['GET', 'POST'],
             '/users/<int:user_id>',
-            self._mock_handler,
+            partial(self._mock_handler, return_value=2),
             self._mock_success_authorizer
         )
+
+        # Add a route to the users endpoint, but without a placeholder, to test
+        # whether the pattern matching is working correctly.
+        router.add_route(
+            ['GET', 'POST'],
+            '/users',
+            partial(self._mock_handler, return_value=3),
+            self._mock_success_authorizer
+        )
+
+        response = router.route('GET', '/users', token=get_access_token(), body={}, ctx=RequestContext())
+        self.assertEqual(response.body['return_value'], 3)
+
+        response = router.route('GET', '/users/1', token=get_access_token(), body={}, ctx=RequestContext())
+        self.assertEqual(response.body['return_value'], 2)
